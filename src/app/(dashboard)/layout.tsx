0a178b01ce2502c2deb05c58fea4466e9d18b043
NataBash <2864tasha@gmail.com>
2025-05-23 16:57:53 +0200

    Создает лайоут для всех страниц

---
+++ src/app/(dashboard)/layout.tsx
@@ -0,0 +1,63 @@
+
+import Nav from "@/components/Nav/Nav";
+import SideBar from "@/components/SideBar/SideBar";
+import Bar from "@/components/Bar/Bar";
+import styles from "@/styles/layout.module.css"
+
+
+
+export default function DashboardLayout({ children }: { children: React.ReactNode }){
+
+    return(
+          <div className={styles.wrapper}>
+      <div className={styles.container}>
+        <main className={styles.main}>
+          <Nav />
+         <div>{children}</div>
+          <SideBar />
+        </main>
+        <Bar />
+        <footer className="footer"></footer>
+      </div>
+    </div>
+    )
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

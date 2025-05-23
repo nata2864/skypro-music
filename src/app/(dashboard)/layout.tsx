92444acda7da1abe2c9dcf90531d71f8be8d37c0
NataBash <2864tasha@gmail.com>
2025-05-23 17:25:18 +0200

    Исправляет ошибку гидротации

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,63 +1,24 @@
+import Nav from '@/components/Nav/Nav';
+import SideBar from '@/components/SideBar/SideBar';
+import Bar from '@/components/Bar/Bar';
+import styles from '@/styles/layout.module.css';
 
-import Nav from "@/components/Nav/Nav";
-import SideBar from "@/components/SideBar/SideBar";
-import Bar from "@/components/Bar/Bar";
-import styles from "@/styles/layout.module.css"
-
-
-
-export default function DashboardLayout({ children }: { children: React.ReactNode }){
-
-    return(
-          <div className={styles.wrapper}>
+export default function DashboardLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className={styles.wrapper}>
       <div className={styles.container}>
         <main className={styles.main}>
           <Nav />
-         <div>{children}</div>
+          <div>{children}</div>
           <SideBar />
         </main>
         <Bar />
         <footer className="footer"></footer>
       </div>
     </div>
-    )
+  );
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,11 +17,7 @@ export const metadata: Metadata = {
   },
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="ru" className={montserrat.variable}>
       <body>{children}</body>

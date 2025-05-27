aa32604ae82c888227bb6c6a595c57ffee10a2af
NataBash <2864tasha@gmail.com>
2025-05-27 15:45:42 +0200

    Добавляет value в Поиске и типизирует

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,17 +1,28 @@
-import styles from "./Seach.module.css";
+'use client';
 
-export default function Search(){
-    return (
-         <div className={styles.centerblock__search}>
-        <svg className={styles.search__svg}>
-          <use xlinkHref="/img/icon/sprite.svg#icon-search"></use>
-        </svg>
-        <input
-          className={styles.search__text}
-          type="search"
-          placeholder="Поиск"
-          name="search"
-        />
-      </div>
-    )
+import styles from './Seach.module.css';
+import { useState } from 'react';
+
+export default function Search() {
+  const [searchInput, setSearchInput] = useState('');
+
+  const onSearchInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchInput(e.target.value);
+  };
+
+  return (
+    <div className={styles.centerblock__search}>
+      <svg className={styles.search__svg}>
+        <use xlinkHref="/img/icon/sprite.svg#icon-search"></use>
+      </svg>
+      <input
+        className={styles.search__text}
+        type="search"
+        placeholder="Поиск"
+        name="search"
+        value={searchInput}
+        onChange={onSearchInput}
+      />
+    </div>
+  );
 }
